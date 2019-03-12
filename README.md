7314e61c50c9873a0572c8c77bc782e668d861a5
Alejandro Roiz Walss <alejandro.roiz.walss@gmail.com>
2019-03-12 01:06:16 -0600

    added controller program

--- README.md
+++ README.md
@@ -8,10 +8,26 @@
 
 Antes de compilar se debe descargar la herramienta [Antlr v4]
 
-Para compilar el archivo g4
+__Se recomienda seguir la [guia de instalación]__
+
+Para compilar el archivo de la gramática (.g4) se utiliza el siguiente comando:
 
 ```java
-java org.antlr.v4.Tool -Dlanguage=Python3 oscar.g4
+java org.antlr.v4.Tool -Dlanguage=Python2 oscar.g4
+```
+O en su defecto:
+
+```java
+antlr4 -Dlanguage=Python2 oscar.g4
+```
+
+De ahora en adelante solo se mostrarán los comandos que utilizan los aliases propuestos en la [guia de instalación]
+
+Para probar el análisis léxico y sintáctico se utiliza el comando:
+
+```python2
+python oscar.py < test.oscar
 ```
 
 [Antlr v4]: https://www.antlr.org/download.html
+[guia de instalación]: https://github.com/antlr/antlr4/blob/master/doc/getting-started.md
--- oscarListener.py
+++ oscarListener.py
@@ -1,397 +1,393 @@
-# Generated from oscar.g4 by ANTLR 4.7.2
+# Generated from oscar.g4 by ANTLR 4.7.1
 from antlr4 import *
-if __name__ is not None and "." in __name__:
-    from .oscarParser import oscarParser
-else:
-    from oscarParser import oscarParser
 
 # This class defines a complete listener for a parse tree produced by oscarParser.
 class oscarListener(ParseTreeListener):
 
     # Enter a parse tree produced by oscarParser#programa.
-    def enterPrograma(self, ctx:oscarParser.ProgramaContext):
+    def enterPrograma(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#programa.
-    def exitPrograma(self, ctx:oscarParser.ProgramaContext):
+    def exitPrograma(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#modulo.
-    def enterModulo(self, ctx:oscarParser.ModuloContext):
+    def enterModulo(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#modulo.
-    def exitModulo(self, ctx:oscarParser.ModuloContext):
+    def exitModulo(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#main.
-    def enterMain(self, ctx:oscarParser.MainContext):
+    def enterMain(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#main.
-    def exitMain(self, ctx:oscarParser.MainContext):
+    def exitMain(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#param.
-    def enterParam(self, ctx:oscarParser.ParamContext):
+    def enterParam(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#param.
-    def exitParam(self, ctx:oscarParser.ParamContext):
+    def exitParam(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#bloque.
-    def enterBloque(self, ctx:oscarParser.BloqueContext):
+    def enterBloque(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#bloque.
-    def exitBloque(self, ctx:oscarParser.BloqueContext):
+    def exitBloque(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#vars_.
-    def enterVars_(self, ctx:oscarParser.Vars_Context):
+    def enterVars_(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#vars_.
-    def exitVars_(self, ctx:oscarParser.Vars_Context):
+    def exitVars_(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#id_.
-    def enterId_(self, ctx:oscarParser.Id_Context):
+    def enterId_(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#id_.
-    def exitId_(self, ctx:oscarParser.Id_Context):
+    def exitId_(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#condicion.
-    def enterCondicion(self, ctx:oscarParser.CondicionContext):
+    def enterCondicion(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#condicion.
-    def exitCondicion(self, ctx:oscarParser.CondicionContext):
+    def exitCondicion(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#escritura.
-    def enterEscritura(self, ctx:oscarParser.EscrituraContext):
+    def enterEscritura(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#escritura.
-    def exitEscritura(self, ctx:oscarParser.EscrituraContext):
+    def exitEscritura(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#lectura.
-    def enterLectura(self, ctx:oscarParser.LecturaContext):
+    def enterLectura(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#lectura.
-    def exitLectura(self, ctx:oscarParser.LecturaContext):
+    def exitLectura(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#expresion.
-    def enterExpresion(self, ctx:oscarParser.ExpresionContext):
+    def enterExpresion(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#expresion.
-    def exitExpresion(self, ctx:oscarParser.ExpresionContext):
+    def exitExpresion(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#exp.
-    def enterExp(self, ctx:oscarParser.ExpContext):
+    def enterExp(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#exp.
-    def exitExp(self, ctx:oscarParser.ExpContext):
+    def exitExp(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#termino.
-    def enterTermino(self, ctx:oscarParser.TerminoContext):
+    def enterTermino(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#termino.
-    def exitTermino(self, ctx:oscarParser.TerminoContext):
+    def exitTermino(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#factor.
-    def enterFactor(self, ctx:oscarParser.FactorContext):
+    def enterFactor(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#factor.
-    def exitFactor(self, ctx:oscarParser.FactorContext):
+    def exitFactor(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#var_cte.
-    def enterVar_cte(self, ctx:oscarParser.Var_cteContext):
+    def enterVar_cte(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#var_cte.
-    def exitVar_cte(self, ctx:oscarParser.Var_cteContext):
+    def exitVar_cte(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#tipo.
-    def enterTipo(self, ctx:oscarParser.TipoContext):
+    def enterTipo(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#tipo.
-    def exitTipo(self, ctx:oscarParser.TipoContext):
+    def exitTipo(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#estatuto.
-    def enterEstatuto(self, ctx:oscarParser.EstatutoContext):
+    def enterEstatuto(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#estatuto.
-    def exitEstatuto(self, ctx:oscarParser.EstatutoContext):
+    def exitEstatuto(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#ciclo.
-    def enterCiclo(self, ctx:oscarParser.CicloContext):
+    def enterCiclo(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#ciclo.
-    def exitCiclo(self, ctx:oscarParser.CicloContext):
+    def exitCiclo(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#estats.
-    def enterEstats(self, ctx:oscarParser.EstatsContext):
+    def enterEstats(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#estats.
-    def exitEstats(self, ctx:oscarParser.EstatsContext):
+    def exitEstats(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#asignacion.
-    def enterAsignacion(self, ctx:oscarParser.AsignacionContext):
+    def enterAsignacion(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#asignacion.
-    def exitAsignacion(self, ctx:oscarParser.AsignacionContext):
+    def exitAsignacion(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#element.
-    def enterElement(self, ctx:oscarParser.ElementContext):
+    def enterElement(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#element.
-    def exitElement(self, ctx:oscarParser.ElementContext):
+    def exitElement(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#llamada.
-    def enterLlamada(self, ctx:oscarParser.LlamadaContext):
+    def enterLlamada(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#llamada.
-    def exitLlamada(self, ctx:oscarParser.LlamadaContext):
+    def exitLlamada(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#mean.
-    def enterMean(self, ctx:oscarParser.MeanContext):
+    def enterMean(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#mean.
-    def exitMean(self, ctx:oscarParser.MeanContext):
+    def exitMean(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#variance.
-    def enterVariance(self, ctx:oscarParser.VarianceContext):
+    def enterVariance(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#variance.
-    def exitVariance(self, ctx:oscarParser.VarianceContext):
+    def exitVariance(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#median.
-    def enterMedian(self, ctx:oscarParser.MedianContext):
+    def enterMedian(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#median.
-    def exitMedian(self, ctx:oscarParser.MedianContext):
+    def exitMedian(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#stdev.
-    def enterStdev(self, ctx:oscarParser.StdevContext):
+    def enterStdev(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#stdev.
-    def exitStdev(self, ctx:oscarParser.StdevContext):
+    def exitStdev(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#head.
-    def enterHead(self, ctx:oscarParser.HeadContext):
+    def enterHead(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#head.
-    def exitHead(self, ctx:oscarParser.HeadContext):
+    def exitHead(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#tail.
-    def enterTail(self, ctx:oscarParser.TailContext):
+    def enterTail(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#tail.
-    def exitTail(self, ctx:oscarParser.TailContext):
+    def exitTail(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#histograma.
-    def enterHistograma(self, ctx:oscarParser.HistogramaContext):
+    def enterHistograma(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#histograma.
-    def exitHistograma(self, ctx:oscarParser.HistogramaContext):
+    def exitHistograma(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#pie_chart.
-    def enterPie_chart(self, ctx:oscarParser.Pie_chartContext):
+    def enterPie_chart(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#pie_chart.
-    def exitPie_chart(self, ctx:oscarParser.Pie_chartContext):
+    def exitPie_chart(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#bar_graph.
-    def enterBar_graph(self, ctx:oscarParser.Bar_graphContext):
+    def enterBar_graph(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#bar_graph.
-    def exitBar_graph(self, ctx:oscarParser.Bar_graphContext):
+    def exitBar_graph(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#union.
-    def enterUnion(self, ctx:oscarParser.UnionContext):
+    def enterUnion(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#union.
-    def exitUnion(self, ctx:oscarParser.UnionContext):
+    def exitUnion(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#intersect.
-    def enterIntersect(self, ctx:oscarParser.IntersectContext):
+    def enterIntersect(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#intersect.
-    def exitIntersect(self, ctx:oscarParser.IntersectContext):
+    def exitIntersect(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#find.
-    def enterFind(self, ctx:oscarParser.FindContext):
+    def enterFind(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#find.
-    def exitFind(self, ctx:oscarParser.FindContext):
+    def exitFind(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#import_csv.
-    def enterImport_csv(self, ctx:oscarParser.Import_csvContext):
+    def enterImport_csv(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#import_csv.
-    def exitImport_csv(self, ctx:oscarParser.Import_csvContext):
+    def exitImport_csv(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#export_csv.
-    def enterExport_csv(self, ctx:oscarParser.Export_csvContext):
+    def enterExport_csv(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#export_csv.
-    def exitExport_csv(self, ctx:oscarParser.Export_csvContext):
+    def exitExport_csv(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#length.
-    def enterLength(self, ctx:oscarParser.LengthContext):
+    def enterLength(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#length.
-    def exitLength(self, ctx:oscarParser.LengthContext):
+    def exitLength(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#min_.
-    def enterMin_(self, ctx:oscarParser.Min_Context):
+    def enterMin_(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#min_.
-    def exitMin_(self, ctx:oscarParser.Min_Context):
+    def exitMin_(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#max_.
-    def enterMax_(self, ctx:oscarParser.Max_Context):
+    def enterMax_(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#max_.
-    def exitMax_(self, ctx:oscarParser.Max_Context):
+    def exitMax_(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#concat.
-    def enterConcat(self, ctx:oscarParser.ConcatContext):
+    def enterConcat(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#concat.
-    def exitConcat(self, ctx:oscarParser.ConcatContext):
+    def exitConcat(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#sort.
-    def enterSort(self, ctx:oscarParser.SortContext):
+    def enterSort(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#sort.
-    def exitSort(self, ctx:oscarParser.SortContext):
+    def exitSort(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#splice.
-    def enterSplice(self, ctx:oscarParser.SpliceContext):
+    def enterSplice(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#splice.
-    def exitSplice(self, ctx:oscarParser.SpliceContext):
+    def exitSplice(self, ctx):
         pass
 
 
     # Enter a parse tree produced by oscarParser#userdef.
-    def enterUserdef(self, ctx:oscarParser.UserdefContext):
+    def enterUserdef(self, ctx):
         pass
 
     # Exit a parse tree produced by oscarParser#userdef.
-    def exitUserdef(self, ctx:oscarParser.UserdefContext):
+    def exitUserdef(self, ctx):
         pass
 
 
