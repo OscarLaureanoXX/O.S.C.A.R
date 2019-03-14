34a0de8ce12732f7e04cda681de2b5985827be1b
Oscar Laureano <oscar.laureano.xx@gmail.com>
2019-03-14 14:42:18 -0600

    Se agregó ejemplo de uso del import con python en el g4 y archivo de prueba

---
+++ oscarVisitor.py
@@ -0,0 +1,222 @@
+# Generated from oscar.g4 by ANTLR 4.7.2
+from antlr4 import *
+
+# This class defines a complete generic visitor for a parse tree produced by oscarParser.
+
+class oscarVisitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by oscarParser#programa.
+    def visitPrograma(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#modulo.
+    def visitModulo(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#main.
+    def visitMain(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#param.
+    def visitParam(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#bloque.
+    def visitBloque(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#vars_.
+    def visitVars_(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#id_.
+    def visitId_(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#condicion.
+    def visitCondicion(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#escritura.
+    def visitEscritura(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#lectura.
+    def visitLectura(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#expresion.
+    def visitExpresion(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#exp.
+    def visitExp(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#termino.
+    def visitTermino(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#factor.
+    def visitFactor(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#var_cte.
+    def visitVar_cte(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#tipo.
+    def visitTipo(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#estatuto.
+    def visitEstatuto(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#ciclo.
+    def visitCiclo(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#estats.
+    def visitEstats(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#asignacion.
+    def visitAsignacion(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#element.
+    def visitElement(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#llamada.
+    def visitLlamada(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#mean.
+    def visitMean(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#variance.
+    def visitVariance(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#median.
+    def visitMedian(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#stdev.
+    def visitStdev(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#head.
+    def visitHead(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#tail.
+    def visitTail(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#histograma.
+    def visitHistograma(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#pie_chart.
+    def visitPie_chart(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#bar_graph.
+    def visitBar_graph(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#union.
+    def visitUnion(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#intersect.
+    def visitIntersect(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#find.
+    def visitFind(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#import_csv.
+    def visitImport_csv(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#export_csv.
+    def visitExport_csv(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#length.
+    def visitLength(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#min_.
+    def visitMin_(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#max_.
+    def visitMax_(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#concat.
+    def visitConcat(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#sort.
+    def visitSort(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#splice.
+    def visitSplice(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by oscarParser#userdef.
+    def visitUserdef(self, ctx):
+        return self.visitChildren(ctx)
+
+
